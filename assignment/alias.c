6555bdb3c154809109c34bf8569a18bec90bb70c
rudgh4493 <70314961+rudgh4493@users.noreply.github.com>
2025-05-27 11:17:41 +0900

    Update alias.c

--- assignment/alias.c
+++ assignment/alias.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-// 명령어 별칭 설정
+
 int main(int argc, char *argv[]) {
     if (argc != 2) {
         printf("사용법: %s '별칭=실제명령어'\n", argv[0]);
@@ -8,8 +8,8 @@ int main(int argc, char *argv[]) {
     }
 
     // 예: alias ll='ls -l'
-    char cmd[100];
-    snprintf(cmd, sizeof(cmd), "alias %s", argv[1]);
+    char cmd[200];  // cmd 크기를 늘려서 긴 명령어를 처리할 수 있도록
+    snprintf(cmd, sizeof(cmd), "alias \"%s\"", argv[1]);  // 큰따옴표로 감싸기
     system(cmd);  // 쉘에 alias 등록
 
     return 0;
